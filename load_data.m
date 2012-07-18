c96688651c790d90dafe842cde5cac8e25da7f75
Justin Willmert <justin@jdjlab.com>
2012-07-18 12:28:57 -0400

    Add map type to load_data.

--- load_data.m
+++ load_data.m
@@ -1,5 +1,5 @@
 function data=load_data(tag, product, vars, varargin)
-%data=load_data(tag, product, varargin)
+%data=load_data(tag, product, vars, varargin)
 %
 %Load various data products in the pipeline based on tag name without the need
 %to generate the path names manually.
@@ -34,13 +34,23 @@ function data=load_data(tag, product, vars, varargin)
 %    string parameter for PRODUCT:
 %
 %        calval    Computed calibration factors
+%        map       Coadded maps
 %
 %
 %    The following listing describes any position-dependent parameters required
-%    for the corresponding data product loading:
+%    for the corresponding data product loading. If an extra parameter is
+%    needed, remember to fill in VARS with either {'*'} or [] to load all
+%    data or the list of desired variables.
 %
 %        calval
 %            None
+%        map
+%            1. coaddopt
+%               Data structure with same form as passed into the
+%               REDUC_COADDPAIRMAP function. The field SERNUM must contain a
+%               serial number as usual, and the JACKTYPE field must be a single
+%               scalar (the default coaddopt specifies all jackknife types)
+%               since a given map type must be chosen.
 %
 %NOTES
 %    1. LOAD_DATA will warn if any variables listed in VARS are unknown for the
@@ -55,12 +65,13 @@ function data=load_data(tag, product, vars, varargin)
     % can be quickly implemented without having to search the function for
     % all relevant uses of a constant string.
 
-    % Initial subdirectory path of each data product
-    PATH_CALDAT = 'data/real/';
+    % Get the initial number of input arguments
+    numargin = nargin;
 
     % Initialize the vars to all vars (with '*') if none are given
     if ~exist('vars', 'var')
         vars = [];
+        numargin = numargin + 1;
     end
     if isempty(vars)
         vars = {'*'};
@@ -74,15 +85,75 @@ function data=load_data(tag, product, vars, varargin)
 
     switch product
         % The general structure of each case should be
-        %     1. Check the list of requested vars with check_vars()
-        %     2. Build a path to the file
-        %     3. Load the data products and return
+        %     1. Verify number of extra parameters passed in
+        %     2. Check the list of requested vars with check_vars()
+        %     3. Build a path to the file
+        %     4. Load the data products and return
 
         case 'calval' % {{{
+            check_nargin(numargin, 0, 'calval');
             check_vars(vars, {'code_dir','en','lc','fs'});
 
-            filename = [PATH_CALDAT tag '_calval.mat'];
+            filename = ['data/real/' tag '_calval.mat'];
 
+            data = load('-mat', filename, vars{:});
+        % }}}
+        case 'map' % {{{
+            check_nargin(numargin, 1, 'map');
+            check_vars(vars, {'coaddopt','m','map'});
+
+            % We require at least a serial number to exist within the coaddopt
+            % struct since it is always a requirement
+            coaddopt = varargin{1};
+            if ~isstruct(coaddopt)
+                error('load_data [map]: ''map'' expects a coaddopt struct');
+            end
+            if ~isfield(coaddopt,'sernum')
+                error(['load_data [map]: Serial number (coaddopt.sernum) '...
+                       'must be given.']);
+            end
+            % Then use the get_default_coaddopt function to combine the given
+            % structure with the required structure members.
+            coaddopt = get_default_coaddopt(coaddopt);
+
+            % If there is more than one jacktype at this stage, we also must
+            % bail since we don't know which one we should load.
+            if length(coaddopt.jacktype) > 1
+                error(['load_data [map]: Only one jackknife type '...
+                       '(coaddopt.jacktype) can be given.']);
+            end
+
+            % Then build the strings required to build the full path. Taken
+            % directly from reduc_coaddpairmaps
+            if iscell(coaddopt.filt)
+                filt = [coaddopt.filt{:}];
+            else
+                filt = coaddopt.filt;
+            end
+            if coaddopt.gs == 1
+                gs = '_gs';
+            else
+                gs = '';
+            end
+            if ~strcmp(coaddopt.proj,'radec')
+                proj = ['_' coaddopt.proj];
+            else
+                proj = '';
+            end
+            if coaddopt.coaddtype > 0
+                coaddtype=sprintf('%1d',coaddopt.coaddtype);
+            else
+                coaddtype='';
+            end
+
+            fileext = sprintf('filt%s_weight%1d%s%s',...
+                        filt,coaddopt.weight,gs,proj);
+            filename = sprintf('maps/%s/%s_%s_%s_jack%d%s.mat',...
+                        coaddopt.sernum(1:4),coaddopt.sernum(5:end),...
+                        coaddopt.daughter,fileext,...
+                        coaddopt.jacktype,coaddtype);
+
+            % Finally load the data
             data = load('-mat', filename, vars{:});
         % }}}
         otherwise
@@ -91,6 +162,14 @@ function data=load_data(tag, product, vars, varargin)
 
     % Restore the warning state
     warning(warnstate);
+
+    function check_nargin(numargin, extra, product)
+        errmsg = nargchk(3+extra, 3+extra, numargin);
+        if length(errmsg) > 0
+            errmsg = ['load_data [' product ']: ' errmsg];
+            error(errmsg);
+        end
+    end
 
     function check_vars(request, known) % {{{
         % Find the intersection of the requested variables with the list of
