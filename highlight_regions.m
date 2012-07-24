e69b09bb0fd5c0f9df9551e434a0028b3e37af01
Justin Willmert <justin@jdjlab.com>
2012-07-24 13:38:04 -0400

    Update highlight_regions to use 2-space tabs.

--- highlight_regions.m
+++ highlight_regions.m
@@ -1,53 +1,52 @@
 function highlight_regions(tag)
 
-    data = load_data(tag, 'tod');
-    load(['data/real/' tag '_tod'])
-    [p,ind] = get_array_info(tag);
+  data = load_data(tag, 'tod');
+  [p,ind] = get_array_info(tag);
 
-    % Chose a good pixel
-    good_pixel = ind.rgl150(50);
+  % Chose a good pixel
+  good_pixel = ind.rgl150(50);
 
-    % Plot once to get the plot range of the data
-    plot(data.d.mce0.data.fb(:,good_pixel));
+  % Plot once to get the plot range of the data
+  plot(data.d.mce0.data.fb(:,good_pixel));
 
-    % Then retrieve the viewport parameters
-    sz = axis;
-    xmin = sz(1);
-    xmax = sz(2);
-    ymin = sz(3);
-    ymax = sz(4);
+  % Then retrieve the viewport parameters
+  sz = axis;
+  xmin = sz(1);
+  xmax = sz(2);
+  ymin = sz(3);
+  ymax = sz(4);
 
-    clf
-    hold on
+  clf
+  hold on
 
-    % Color the elnod regions
-    for i=[1:length(data.en.sf)]
-        rectangle('Position',[data.en.sf(i), ymin, ...
-                    (data.en.ef(i)-data.en.sf(i)), (ymax-ymin)],...
-                'FaceColor',[.9 1 .67],'EdgeColor','none')
-    end
+  % Color the elnod regions
+  for i=[1:length(data.en.sf)]
+    rectangle('Position',[data.en.sf(i), ymin, ...
+          (data.en.ef(i)-data.en.sf(i)), (ymax-ymin)],...
+        'FaceColor',[.9 1 .67],'EdgeColor','none')
+  end
 
-    % Color the load curve regions
-    for i=[1:length(data.lc.sf)]
-        rectangle('Position',[data.lc.sf(i), ymin, ...
-                    (data.lc.ef(i)-data.lc.sf(i)) (ymax-ymin)],...
-                'FaceColor',[1 .88 0.67],'EdgeColor','none')
-    end
+  % Color the load curve regions
+  for i=[1:length(data.lc.sf)]
+    rectangle('Position',[data.lc.sf(i), ymin, ...
+          (data.lc.ef(i)-data.lc.sf(i)) (ymax-ymin)],...
+        'FaceColor',[1 .88 0.67],'EdgeColor','none')
+  end
 
-    % Color the half-scan regions
-    for i=[1:length(data.fs.sf)]
-        rectangle('Position',[data.fs.sf(i), ymin, ...
-                    (data.fs.ef(i)-data.fs.sf(i)) (ymax-ymin)],...
-                'FaceColor',[.69 .86 1],'EdgeColor','none')
-    end
+  % Color the half-scan regions
+  for i=[1:length(data.fs.sf)]
+    rectangle('Position',[data.fs.sf(i), ymin, ...
+          (data.fs.ef(i)-data.fs.sf(i)) (ymax-ymin)],...
+        'FaceColor',[.69 .86 1],'EdgeColor','none')
+  end
 
-    % Then finally plot the actual data on top
-    plot(data.d.mce0.data.fb(:,good_pixel))
+  % Then finally plot the actual data on top
+  plot(data.d.mce0.data.fb(:,good_pixel))
 
-    title(['Highlighted regions for pixel ' num2str(good_pixel)])
-    xlabel('Timeseries (as data points)')
-    ylabel('Feedback signal')
+  title(['Highlighted regions for pixel ' num2str(good_pixel)])
+  xlabel('Timeseries (as data points)')
+  ylabel('Feedback signal')
 
-    hold off
+  hold off
 end
 
