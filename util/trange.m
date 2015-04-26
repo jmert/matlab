1391840752448d75e92ddf8344f049cfcd6f38f9
Justin Willmert <justin@jdjlab.com>
2015-04-26 16:27:42 -0400

    [trange] Option to select either fast or slow times.

--- util/trange.m
+++ util/trange.m
@@ -1,5 +1,5 @@
 function [is,ie]=trange(d,tstart,tend,fast,format)
-% [is,ie]=trange(d,tstart,tend)
+% [is,ie]=trange(d,tstart,tend,fast,format)
 % 
 % Searches d.t or d.ts for the indices corresponding closest to the given
 % beginning and end times.
