4cf267471509e604f3ffe8a8baa64c4744658927
Justin Willmert <willmert@physics.umn.edu>
2016-02-25 11:06:37 -0500

    [summarize_job] Correctly use accumulated data.
    Also make more robust against errors.

--- util/summarize_job.m
+++ util/summarize_job.m
@@ -27,17 +27,16 @@ function stats=summarize_job(jobregex,datebegin,dateend)
   if ischar(datebegin)
     datebegin = datenum(datebegin);
   end
-  if ischar(datened)
+  if ischar(dateend)
     dateend = datenum(dateend);
   end
 
   datebegin = floor(datebegin);
   dateend = ceil(dateend)-1;
 
-  % Column which contains JobName field
-  jn = strmatch('JobName',xx.fields(:,1));
-
+  stats = struct();
   info = {};
+  fields = {};
   for dd=datebegin:dateend
     datestamp = datestr(dd, 'yyyymmdd');
     statfile = sprintf('farmfiles/stats/%s.mat', datestamp);
@@ -49,45 +48,51 @@ function stats=summarize_job(jobregex,datebegin,dateend)
     fprintf(1,'reading statistics for %s...\n', datestamp);
     xx = load(statfile);
 
+    if isempty(fields)
+      fields = xx.fields;
+      jn = strmatch('JobName',fields(:,1));
+    end
     % Filter away all entries which don't match the given regular expression
     matches = ~cellfun(@isempty, regexp(xx.info(:,jn), jobregex));
     info = [info; xx.info(matches,:)];
   end
+  if isempty(info)
+    disp('No data to analyze.')
+    return
+  end
 
-  stats = struct();
-
-  sn = strmatch('State', xx.fields(:,1));
+  sn = strmatch('State', fields(:,1));
   % Mask for successfully completed jobs
-  maskcd = rvec(strcmp('COMPLETED', xx.info(:,sn)));
+  maskcd = rvec(strcmp('COMPLETED', info(:,sn)));
   % Mask for failed jobs
-  maskf  = rvec(strcmp('FAILED', xx.info(:,sn)));
+  maskf  = rvec(strcmp('FAILED', info(:,sn)));
   % Mask for timed-out jobs
-  maskto = rvec(strcmp('TIMEOUT', xx.info(:,sn)));
+  maskto = rvec(strcmp('TIMEOUT', info(:,sn)));
 
   stats.numcomplete = sum(maskcd);
   stats.numfail = sum(maskf);
   stats.numtimeout = sum(maskto);
 
-  nn = strmatch('MaxVMSize', xx.fields(:,1));
+  nn = strmatch('MaxVMSize', fields(:,1));
   if ~isempty(nn)
-    stats.memuse.avg = mean(horzcat(xx.info{maskcd,nn}));
-    stats.memuse.max = max(horzcat(xx.info{maskcd,nn}));
+    stats.memuse.avg = mean(horzcat(info{maskcd,nn}));
+    stats.memuse.max = max(horzcat(info{maskcd,nn}));
 
     % Find an even number of base-10 gigabytes to histogram across.
     gb = ceil(stats.memuse.max/1000)*1000;
-    [bc,cnt] = hfill(horzcat(xx.info{maskcd,nn}), 100, 0, gb);
+    [bc,cnt] = hfill(horzcat(info{maskcd,nn}), 100, 0, gb);
     stats.memuse.hist.bc = bc;
     stats.memuse.hist.n  = cnt;
   end
 
-  nn = strmatch('Elapsed', xx.fields(:,1));
+  nn = strmatch('Elapsed', fields(:,1));
   if ~isempty(nn)
-    stats.timeuse.avg = mean(horzcat(xx.info{maskcd,nn}));
-    stats.timeuse.max = max(horzcat(xx.info{maskcd,nn}));
+    stats.timeuse.avg = mean(horzcat(info{maskcd,nn}));
+    stats.timeuse.max = max(horzcat(info{maskcd,nn}));
 
     % Find nearest quarter hour to histogram across.
     qhr = ceil(stats.timeuse.max/15)*15;
-    [bc,cnt] = hfill(horzcat(xx.info{maskcd,nn}), 100, 0, qhr);
+    [bc,cnt] = hfill(horzcat(info{maskcd,nn}), 100, 0, qhr);
     stats.timeuse.hist.bc = bc;
     stats.timeuse.hist.n  = cnt;
   end
