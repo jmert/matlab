5e68276d27f8b45242bea0efe951c1a29cd734c8
Justin Willmert <willmert@physics.umn.edu>
2016-07-13 16:14:06 -0400

    [flatten] Option to remove empty elements, true by default.

--- extensions/flatten.m
+++ extensions/flatten.m
@@ -1,8 +1,18 @@
-function A=flatten(A)
-% function A=flatten(A)
+function A=flatten(A,squash)
+% function A=flatten(A,squash)
 %
 % Flattens a cell array such that all members are non-cell arrays.
 %
+% INPUTS
+%   A         The cell array to flatten.
+%
+%   squash    Optional, defaults to true. If false, then empty array
+%             entries are not removed from the output.
+%
+
+  if ~exist('squash','var') || isempty(squash)
+    squash = true;
+  end
 
   if ~iscell(A)
     return
@@ -14,6 +24,10 @@ function A=flatten(A)
     A = [A(1:(idx-1)), A{idx}, A((idx+1):end)];
 
     cells = cellfun(@iscell, A);
+  end
+
+  if squash
+    A = A(~cellfun(@isempty, A));
   end
 end
 
