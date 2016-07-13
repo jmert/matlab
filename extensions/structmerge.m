a64139f222a7c12ff9dee8f009ff455ecac000be
Justin Willmert <willmert@physics.umn.edu>
2016-07-13 12:52:43 -0400

    [structmerge] Simple util to merge fields of two structures.

---
+++ extensions/structmerge.m
@@ -0,0 +1,26 @@
+function C=structmerge(A,B)
+% C=structmerge(A,B)
+%
+% Merges the contents of B into A, overwriting any common fields between A and
+% B with the values in B, returning the combined result in C.
+%
+
+  % Allow A or B to be empty and act as a no-op, passing through the other
+  % value.
+  if isempty(A)
+    C = B;
+    return
+  elseif isempty(B)
+    C = A;
+    return
+  end
+
+  % Get a copy of A with fields specified in B removed.
+  C = rmfield(A, intersect(fieldnames(A), fieldnames(B)));
+  % Then copy in the values from B to C.
+  for ff=rvec(fieldnames(B))
+    C.(ff{:}) = B.(ff{:});
+  end
+
+end
+
