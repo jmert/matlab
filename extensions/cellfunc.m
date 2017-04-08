3145c808b09f644d56c67b5b7fb51a5e2b7824b9
Justin Willmert <justin@jdjlab.com>
2017-04-08 12:47:12 -0500

    [arrayfunc,cellfunc] Remove since now committed to main analysis pipeline

--- extensions/cellfunc.m
+++
@@ -1,8 +0,0 @@
-function varargout=cellfunc(varargin)
-% function varargout=cellfunc(varargin)
-%
-% Wrapper around cellfun() which appends "'uniformoutput',false" as arguments
-% to cellfun().
-%
-  [varargout{1:nargout}] = cellfun(varargin{:}, 'uniformoutput', false);
-end
