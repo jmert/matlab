44c96006d94692043522a2fb622906a288188786
Justin Willmert <willmert@physics.umn.edu>
2017-09-06 18:08:00 -0400

    [flatten] Handle differing-shaped inputs more consistently

--- extensions/flatten.m
+++ extensions/flatten.m
@@ -1,7 +1,8 @@
 function A=flatten(A,squash)
 % function A=flatten(A,squash)
 %
-% Flattens a cell array such that all members are non-cell arrays.
+% Flattens a cell array such that all members are non-cell arrays. Note that
+% shape is not preserved --- all inputs are returned as a row-vector.
 %
 % INPUTS
 %   A         The cell array to flatten.
@@ -18,10 +19,12 @@ function A=flatten(A,squash)
     return
   end
 
+  A = reshape(A, 1, []);
+
   cells = cellfun(@iscell, A);
   while any(cells)
     idx = find(cells, 1, 'first');
-    A = [A(1:(idx-1)), A{idx}, A((idx+1):end)];
+    A = [A(1:(idx-1)), reshape(A{idx},1,[]), A((idx+1):end)];
 
     cells = cellfun(@iscell, A);
   end
