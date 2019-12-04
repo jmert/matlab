0613534d004d22875b5c4fee4a8b05ddf9ba399a
Justin Willmert <willmert@physics.umn.edu>
2019-12-03 18:28:02 -0600

    [colormap_interp] Use gamma-corrected RGB interpolation if LAB colorspace is unavailable

--- plotting/colormap_interp.m
+++ plotting/colormap_interp.m
@@ -1,5 +1,5 @@
-function cmap=colormap_interp(cmap,n)
-% cmap=colormap_interp(cmap,n)
+function cmap=colormap_interp(cmap,n,space)
+% cmap=colormap_interp(cmap,n,space)
 %
 % Interpolates the colormap from its given number of entries to n entries.
 % Interpolation is done in the L*a*b colorspace (except when rgb2lab() fails
@@ -12,17 +12,42 @@ function cmap=colormap_interp(cmap,n)
     return
   end
 
-  labinterp = true;
-  try
-    cmap = rgb2lab(cmap);
-  catch
-    labinterp = false;
+  if ~exist('space','var') || isempty(space)
+    if exist('rgb2lab','file')
+      space = 'lab';
+    else
+      space = 'gamma';
+    end
   end
+  switch space
+    case {'linear','rgb'}
+      trans = @id;
+      invtrans = @invid;
+    case 'gamma'
+      trans = @gamma;
+      invtrans = @invgamma;
+    case 'lab'
+      trans = @rgb2lab;
+      invtrans = @lab2rgb;
+    otherwise
+      error('unknown interpolation space: %s', space);
+  end
+
+  cmap = trans(cmap);
   cmap = interp1(linspace(1, n, m), cmap, 1:n);
-  if labinterp
-    cmap = lab2rgb(cmap);
-    % clamp within range
-    cmap(cmap < 0) = 0;
-    cmap(cmap > 1) = 1;
-  end
+  cmap = invtrans(cmap);
+  cmap(cmap < 0) = 0;
+  cmap(cmap > 1) = 1;
+end
+
+function x=id(x)
+end
+function x=invid(x)
+end
+
+function x=gamma(x)
+  x = x .^ 2.2;
+end
+function x=invgamma(x)
+  x = x .^ (1/2.2);
 end
